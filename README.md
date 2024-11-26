8a5a96206453eaf2e5dee3d5dac10586b1b30f9d
Sushant Kumar <sushantkumar23@gmail.com>
2024-11-26 17:51:46 +0530

    Abstracted Hyperparameters

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
+# MicroLM
+
+This repository contains a PyTorch implementation of the MicroLM model. Improvement over the original nanogpt implementation by Andrej Karpathy are as follows:
+
+1. Rotary embeddings: are used for the attention mechanism
+
+## Setup
+
+Run the following commands to setup the environment and run the training script.
+
 ```
 git clone https://github.com/sushantkumar23/microlm
 cd microlm
 pip install -r requirements.txt
 pip install --pre torch --index-url https://download.pytorch.org/whl/nightly/cu124 --upgrade
 python cached_fineweb10B.py 10
-chmod +x run.sh
-./run.sh
+torchrun --standalone --nproc_per_node=2 pretrain_v1.py
 ```
+
+## Loss Visualization
+
+Jupyter notebook `Loss_Plot.ipynb` is provided to visualize the loss curves.
--- pretrain_v1.py
+++ pretrain_v1.py
@@ -250,54 +250,6 @@ import tiktoken
 import numpy as np
 
 
-def load_tokens(filename):
-    npt = np.load(filename)
-    npt = npt.astype(np.int32)  # added after video
-    ptt = torch.tensor(npt, dtype=torch.long)
-    return ptt
-
-
-class DataLoaderLite:
-    def __init__(self, B, T, process_rank, num_processes, split):
-        self.B = B
-        self.T = T
-        self.process_rank = process_rank
-        self.num_processes = num_processes
-        assert split in {"train", "val"}
-
-        # get the shard filenames
-        data_root = "edu_fineweb10B"
-        shards = os.listdir(data_root)
-        shards = [s for s in shards if split in s]
-        shards = sorted(shards)
-        shards = [os.path.join(data_root, s) for s in shards]
-        self.shards = shards
-        assert len(shards) > 0, f"no shards found for split {split}"
-        if master_process:
-            print(f"found {len(shards)} shards for split {split}")
-        self.reset()
-
-    def reset(self):
-        # state, init at shard zero
-        self.current_shard = 0
-        self.tokens = load_tokens(self.shards[self.current_shard])
-        self.current_position = self.B * self.T * self.process_rank
-
-    def next_batch(self):
-        B, T = self.B, self.T
-        buf = self.tokens[self.current_position : self.current_position + B * T + 1]
-        x = (buf[:-1]).view(B, T)  # inputs
-        y = (buf[1:]).view(B, T)  # targets
-        # advance the position in the tensor
-        self.current_position += B * T * self.num_processes
-        # if loading the next batch would be out of bounds, advance to next shard
-        if self.current_position + (B * T * self.num_processes + 1) > len(self.tokens):
-            self.current_shard = (self.current_shard + 1) % len(self.shards)
-            self.tokens = load_tokens(self.shards[self.current_shard])
-            self.current_position = B * T * self.process_rank
-        return x, y
-
-
 def _peek_data_shard(filename):
     # only reads the header, returns header data
     with open(filename, "rb") as f:
@@ -414,6 +366,17 @@ def get_most_likely_row(tokens, mask, logits):
 # DDP launch for e.g. 8 GPUs:
 # torchrun --standalone --nproc_per_node=8 train_gpt2.py
 
+
+@dataclass
+class Hyperparameters:
+    train_shards_pattern: str = "data/fineweb10B/fineweb_train_*.bin"
+    val_shards_pattern: str = "data/fineweb10B/fineweb_val_*.bin"
+
+    val_loss_every: int = 125
+
+
+args = Hyperparameters()
+
 # run the training loop
 from torch.distributed import init_process_group, destroy_process_group
 from torch.nn.parallel import DistributedDataParallel as DDP
@@ -456,7 +419,10 @@ if torch.cuda.is_available():
 enc = tiktoken.get_encoding("gpt2")
 
 total_batch_size = 524288  # 2**19, ~0.5M, in number of tokens
-B = 64  # micro batch size
+
+# Keep batch size small for MPS: Macbook
+B = 8 if device == "mps" else 64  # micro batch size
+
 T = 1024  # sequence length
 assert (
     total_batch_size % (B * T * ddp_world_size) == 0
@@ -467,12 +433,12 @@ if master_process:
     print(f"=> calculated gradient accumulation steps: {grad_accum_steps}")
 
 # load tokens
-input_train_bin: str = "data/fineweb10B/fineweb_train_*.bin"  # input .bin to train on
-input_val_bin: str = (
-    "data/fineweb10B/fineweb_val_*.bin"  # input .bin to eval validation loss on
+train_loader = DistributedDataLoader(
+    args.train_shards_pattern, B, T, ddp_rank, ddp_world_size
 )
-train_loader = DistributedDataLoader(input_train_bin, B, T, ddp_rank, ddp_world_size)
-val_loader = DistributedDataLoader(input_val_bin, B, T, ddp_rank, ddp_world_size)
+val_loader = DistributedDataLoader(
+    args.val_shards_pattern, B, T, ddp_rank, ddp_world_size
+)
 
 torch.set_float32_matmul_precision("high")
 
@@ -530,7 +496,7 @@ for step in range(max_steps):
     last_step = step == max_steps - 1
 
     # once in a while evaluate our validation loss
-    if step % 250 == 0 or last_step:
+    if step % args.val_loss_every == 0 or last_step:
         model.eval()
         val_loader.reset()
         with torch.no_grad():
@@ -539,7 +505,6 @@ for step in range(max_steps):
             for _ in range(val_loss_steps):
                 x, y = val_loader.next_batch()
                 x, y = x.to(device), y.to(device)
-                print(x.shape, y.shape)
                 with torch.autocast(device_type=device_type, dtype=torch.bfloat16):
                     logits, loss = model(x, y)
                 loss = loss / val_loss_steps
