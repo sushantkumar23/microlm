72b3d0a8ba5ec01cdc123135eae296af11b57d93
Sushant Kumar <sushantkumar23@gmail.com>
2024-11-26 18:19:00 +0530

    Fixed the issue with missing head_dim

--- pretrain.py
+++ pretrain.py
@@ -53,6 +53,7 @@ class CausalSelfAttention(nn.Module):
     def __init__(self, config):
         super().__init__()
         assert config.n_embd % config.n_head == 0
+        self.head_dim = config.n_embd // config.n_head
         # key, query, value projections for all heads, but in a batch
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
         # output projection
